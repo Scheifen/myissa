7178955a68d34f7bb49531b742f1c7abdd6b42c1
Scheifen <scheifen@gmail.com>
2020-06-26 12:00:07 -0500

    Init

---
+++ General/embeddded JS.js
@@ -0,0 +1,102 @@
+function checkURL(url) {
+    var orig = window.location.href.split("issassa/")[1]
+    var apex = orig.search("f?p=")
+    if (apex != "-1") {
+        if (orig.search(url) != "-1") {
+            return "Y";
+        } else {
+            return "N";
+        }
+    } else {
+        if (orig.search(url) != "-1") {
+            return "Y";
+        } else {
+            return "N";
+        };
+    };
+};
+
+function metaAjax() {
+    if (checkURL("p_product_serno=1515") == "Y") {
+        $.ajax({
+            url: "/images/My_Images/metaTags.html",
+            success(result) {
+                $("title").after(result);
+            }
+        })
+        $("title").text("GBAC Fundamentals Online Course")
+    }
+};
+        
+$('document').ready(function () {
+metaAjax();
+$('<html>').attr
+
+        // RESTYLE THE DROPDOWN MENU
+    $('#google_translate_element').on("click", function () {
+
+        // Change font family and color
+        $("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")
+            .css({
+                'color': '#544F4B',
+                'font-family': 'Roboto',
+                                'width':'100%'
+            });
+        // Change menu's padding
+        $("iframe").contents().find('.goog-te-menu2-item-selected').css ('display', 'none');
+            
+                // Change menu's padding
+        $("iframe").contents().find('.goog-te-menu2').css ('padding', '0px');
+      
+        // Change the padding of the languages
+        $("iframe").contents().find('.goog-te-menu2-item div').css('padding', '20px');
+      
+        // Change the width of the languages
+        $("iframe").contents().find('.goog-te-menu2-item').css('width', '100%');
+        $("iframe").contents().find('td').css('width', '100%');
+      
+        // Change hover effects
+        $("iframe").contents().find(".goog-te-menu2-item div").hover(function () {
+            $(this).css('background-color', '#4385F5').find('span.text').css('color', 'white');
+        }, function () {
+            $(this).css('background-color', 'white').find('span.text').css('color', '#544F4B');
+        });
+
+        // Change Google's default blue border
+        $("iframe").contents().find('.goog-te-menu2').css('border', 'none');
+
+        // Change the iframe's box shadow
+        $(".goog-te-menu-frame").css('box-shadow', '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3)');
+        
+      
+      
+        // Change the iframe's size and position?
+        $(".goog-te-menu-frame").css({
+            'height': '100%',
+            'width': '100%',
+            'top': '0px'
+        });
+        // Change iframes's size
+        $("iframe").contents().find('.goog-te-menu2').css({
+            'height': '100%',
+            'width': '100%'
+        });
+    });
+});
+
+function readCookie(cname) {
+    var x = decodeURIComponent(document.cookie);
+    var y = x.split(';');
+    var name = cname + "=";
+
+    for (var i = y.length-1; i > 0; i--) {
+    	var c = y[i];
+    	while (c.charAt(0) == ' ') {
+    		c = c.substring(1);
+    	} 
+    	if (c.indexOf(name) == 0) {
+    		return c.substring(name.length, c.length);
+    	}
+    }
+    return "";
+};
